33337ca402e1144b3d9d3b6b89b6f0fe3ef89043
sohel5G <contactsohelrana>
2023-08-17 12:58:55 +0600

    Login page done

---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+document.getElementById('login-submit-btn').addEventListener('click', function(){
+    let mailInputValue = document.getElementById('mail-input').value;
+    let passInputValue = document.getElementById('pass-input').value;
+
+    if( mailInputValue === 'admin@admin.com' && passInputValue === 'admin'){
+        window.location.href = 'dashboard.html';
+    }
+
+});
+
